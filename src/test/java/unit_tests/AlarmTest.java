98b019baf6df7b80744b2b29a14065c89e3b9d73
Antonio de la Torre <antondelatorre@gmail.com>
2018-05-25 00:15:09 +0200

    Fire alarm when pressure is too low

--- src/test/java/unit_tests/AlarmTest.java
+++ src/test/java/unit_tests/AlarmTest.java
@@ -1,6 +1,7 @@
 package unit_tests;
 
 import org.junit.Test;
+import tirepressuremonitoringsystem.Alarm;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
@@ -8,8 +9,24 @@ import static org.hamcrest.Matchers.is;
 public class AlarmTest {
 
     @Test
-    public void fix_me_and_rename_ne() {
-        assertThat(true, is(false));
+    public void fire_alarm_when_pressure_is_too_low() {
+        FakeAlarm alarm = new FakeAlarm();
+        alarm.check();
+        assertThat(alarm.out, is("Alarm activated!"));
+    }
+
+    private class FakeAlarm extends Alarm {
+        public String out;
+
+        @Override
+        protected void print(String message) {
+            out = message;
+        }
+
+        @Override
+        protected double getPressure() {
+            return 16;
+        }
     }
 }
 
